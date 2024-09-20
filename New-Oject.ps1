2ace66c22f43bc0870fd9eabc4627f0a0c1491db
Suresh <66471310+devopstraining1990@users.noreply.github.com>
2024-09-20 22:50:35 +0530

    Update New-Oject.ps1

--- New-Oject.ps1
+++ New-Oject.ps1
@@ -3,9 +3,5 @@ $custiomobject = @{
     lastname  = "meenige"
 }
 
- testd
 $outobj = New-Object psobject -Property $custiomobject; jhghtuyymjyuy
-
-$outobj = New-Object psobject -Property $custiomobject;
- main
 Write-Host $outobj | Format-List 
